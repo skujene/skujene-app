561868d509c1a58c03f2b6d3c5775163f4b55df1
Arturs Demiters <7239988+demiters@users.noreply.github.com>
2023-04-28 20:41:48 +0300

    config edge runtime

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,10 @@
+import type { NextRequest } from 'next/server'
+// import Google Sheets or take req params
+
+export const config = {
+  runtime: 'edge',
+};
+
 export async function GET(request: Request) {
   return new Response('Hello, Next.js!')
 }
