02ebeb2aceef00f1121046964b862d260a80968e
Arturs Demiters <7239988+demiters@users.noreply.github.com>
2023-04-28 21:25:09 +0300

    more edge runtime config

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const config = {
+  runtime: "edge",
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
